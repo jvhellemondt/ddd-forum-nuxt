543a1359c1937ff957a5c8a256b723339ca28a6a
Jens van Hellemondt <jens@invictus.codes>
2024-08-29 10:32:26 +0200

    feat: api facade

---
+++ src/composables/api/useApi.ts
@@ -0,0 +1,33 @@
+import type { RegistrationInput } from '../../types/auth/register/RegistrationInput'
+
+export const useApi = () => {
+  return {
+    posts: {
+      getPosts: (): Post[] => {
+        return [
+          {
+            title: 'My first post!',
+            votes: [
+              { id: 1, postId: 1, voteType: 'Upvote' },
+              { id: 2, postId: 1, voteType: 'Downvote' },
+              { id: 3, postId: 1, voteType: 'Upvote' },
+              { id: 4, postId: 1, voteType: 'Upvote' },
+            ],
+            dateCreated: new Date("2024-08-25"),
+            memberPostedBy: {
+              user: {
+                username: 'Khalil'
+              }
+            },
+            comments: [],
+          },
+        ]
+      }
+    },
+    register: (input: RegistrationInput) => {
+      return input
+    }
+  }
+
+  // const { data, status, error, refresh, clear } = await useFetch('/api/auth/login')
+};
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -8,22 +8,6 @@ import type { Post } from '~/types/Post'
 const title = ref('Home')
 useHeadSafe({ title })
 
-const posts: Post[] = [
-  {
-    title: 'My first post!',
-    votes: [
-      { id: 1, postId: 1, voteType: 'Upvote' },
-      { id: 2, postId: 1, voteType: 'Downvote' },
-      { id: 3, postId: 1, voteType: 'Upvote' },
-      { id: 4, postId: 1, voteType: 'Upvote' },
-    ],
-    dateCreated: new Date("2024-08-25"),
-    memberPostedBy: {
-      user: {
-        username: 'Khalil'
-      }
-    },
-    comments: [],
-  },
-]
+const api = useApi();
+const posts: Post[] = await api.posts.getPosts()
 </script>
