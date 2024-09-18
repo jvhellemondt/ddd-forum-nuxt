97124e992c760e48ac15faf74e56d0e083f79e29
Jens van Hellemondt <jens@invictus.codes>
2024-09-18 21:09:33 +0200

    fix(api): properly pass on the request to backend

--- server/api/[...].ts
+++ server/api/[...].ts
@@ -5,16 +5,13 @@ export default defineEventHandler(async (event) => {
   const apiPrefix = 'api/';
   const path = req.url.slice(apiPrefix.length)
   const url = new URL(path, baseUrl).toString()
-
-  const response = await $fetch(url, {
+  const body = req.method?.toLowerCase() !== 'get' ? await readBody(event) : undefined
+  const response = await fetch(url, {
     method: req.method,
     headers: req.headers as HeadersInit,
-    body: req.method?.toLowerCase() !== 'get' ? req : undefined,
-    duplex: 'half',
-    onResponse({ response }) {
-      event.res.setHeader('Content-Type', response.headers.get('content-type') || 'text/plain');
-      event.res.statusCode = response.status;
-    }
+    body: JSON.stringify(body),
   });
+  event.res.setHeader('Content-Type', response.headers.get('content-type') || 'text/plain');
+  setResponseStatus(event, response.status);
   return response;
 });
